10b98d0cb6be78b7e84b81f8c4ed55eeecc63b1d
im-usb <udaysbaghel@gmail.com>
2023-09-02 17:59:31 +0530

    JS code using async-await

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -3,18 +3,31 @@ const jokeBtn = document.getElementById("jokeBtn");
 const pTag = document.createElement("p");
 joke.appendChild(pTag);
 
-function generateJoke() {
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   fetch("https://icanhazdadjoke.com/", config)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       pTag.innerHTML = data.joke;
+//     });
+// }
+
+//using async-await
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
 
-  fetch("https://icanhazdadjoke.com/", config)
-    .then((response) => response.json())
-    .then((data) => {
-      pTag.innerHTML = data.joke;
-    });
+  const res = await fetch("https://icanhazdadjoke.com/", config);
+  const data = await res.json();
+  pTag.innerHTML = data.joke;
 }
 
 generateJoke();
